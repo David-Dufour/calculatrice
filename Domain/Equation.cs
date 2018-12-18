a134161ce1b942371180f433dedfb8555528dd33
David Dufour <ada.david.dufour@gmail.com>
2018-12-17 22:20:07 -0500

    Ajout d'une classe Equation qui sera responsable de résoudre sa propre équation.

---
+++ Domain/Equation.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Calculatrice.Domain
+{
+  public class Equation
+  {
+    private readonly Calculator calculator = new Calculator();
+    private readonly string equation;
+
+    public Equation(string equation)
+    {
+      this.equation = equation;
+    }
+
+    private double? result;
+    public double Result
+    {
+      get
+      {
+        if (!result.HasValue)
+        {
+          result = Solve(equation);
+        }
+
+        return result.Value;
+      }
+    }
+
+    private double Solve(string equation)
+    {
+      return 0;
+    }
+  }
+}
